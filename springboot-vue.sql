bd604116cca7053686f19bc645bce5f8dafe5a3e
一个魂 <2460033107@qq.com>
2021-11-09 15:11:08 +0800

    1

--- springboot-vue.sql
+++ springboot-vue.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50721
+ Source Server Version : 80025
  Source Host           : localhost:3306
  Source Schema         : springboot-vue
 
  Target Server Type    : MySQL
- Target Server Version : 50721
+ Target Server Version : 80025
  File Encoding         : 65001
 
- Date: 12/09/2021 23:32:53
+ Date: 09/11/2021 15:05:45
 */
 
 SET NAMES utf8mb4;
@@ -22,9 +22,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `area`;
 CREATE TABLE `area`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(0) NOT NULL AUTO_INCREMENT,
   `label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `pid` int(11) NULL DEFAULT NULL,
+  `pid` int(0) NULL DEFAULT NULL,
   `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
@@ -41,15 +41,15 @@ INSERT INTO `area` VALUES (3, '政务区', 2, 'Zhengwu');
 -- ----------------------------
 DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
   `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '价格',
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '作者',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '出版日期',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '出版日期',
   `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '封面地址',
-  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of book
@@ -61,9 +61,9 @@ INSERT INTO `book` VALUES (1, '明朝那些事', 60.00, 'xxx', '2010-07-01 00:00
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
-  `pid` int(11) NULL DEFAULT NULL COMMENT '父节点id',
+  `pid` int(0) NULL DEFAULT NULL COMMENT '父节点id',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
@@ -82,90 +82,88 @@ INSERT INTO `category` VALUES (7, '法律', 3);
 -- ----------------------------
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '内容',
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '评论人',
   `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '评论时间',
-  `parent_id` bigint(20) NULL DEFAULT NULL COMMENT '父ID',
-  `foreign_id` bigint(20) NULL DEFAULT 0 COMMENT '关联id',
+  `parent_id` bigint(0) NULL DEFAULT NULL COMMENT '父ID',
+  `foreign_id` bigint(0) NULL DEFAULT 0 COMMENT '关联id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '留言表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '留言表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of message
 -- ----------------------------
-INSERT INTO `message` VALUES (20, '哈哈哈', 'admin', '2021-05-22 10:48:55', NULL, 0);
-INSERT INTO `message` VALUES (21, '哦豁', 'admin', '2021-05-22 10:49:48', NULL, 0);
-INSERT INTO `message` VALUES (22, '老弟', 'admin', '2021-05-22 10:51:07', 21, 0);
-INSERT INTO `message` VALUES (23, '哈哈哈', 'zhang', '2021-05-24 17:13:45', 22, 0);
-INSERT INTO `message` VALUES (24, '我们都爱吃大西瓜', 'zhang', '2021-05-24 17:13:58', NULL, 0);
+INSERT INTO `message` VALUES (20, '哈哈哈', 'admin', '2020-05-22 10:48:55', NULL, 0);
+INSERT INTO `message` VALUES (21, '哦豁', 'admin', '2020-05-22 10:49:48', NULL, 0);
+INSERT INTO `message` VALUES (22, '老弟', 'admin', '2020-05-22 10:51:07', 21, 0);
+INSERT INTO `message` VALUES (23, '哈哈哈', 'zhang', '2020-05-24 17:13:45', 22, 0);
+INSERT INTO `message` VALUES (24, '我们都爱吃大西瓜', 'zhang', '2020-05-24 17:13:58', NULL, 0);
 
 -- ----------------------------
 -- Table structure for news
 -- ----------------------------
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标题',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '内容',
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '作者',
-  `time` datetime NULL DEFAULT NULL COMMENT '发布时间',
+  `time` datetime(0) NULL DEFAULT NULL COMMENT '发布时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of news
 -- ----------------------------
-INSERT INTO `news` VALUES (4, '4444', '<p>444444</p><p><br/></p><p>44444</p><p><br/></p><p><br/></p><p>555555<img src=\"https://img1.baidu.com/it/u=4110196045,3829597861&amp;fm=26&amp;fmt=auto&amp;gp=0.jpg\" contenteditable=\"false\" style=\"font-size: 14px; max-width: 100%;\"/></p>', '管理员', '2021-07-17 18:55:27');
-INSERT INTO `news` VALUES (5, '77777', '<p>7777</p><p><br/></p><p><img src=\"http://localhost:9090/files/c536f6f2f0e94983951240d73d740601\" style=\"max-width:100%;\" contenteditable=\"false\" width=\"388\" height=\"388\"/><br/></p><p>8888</p><p><br/></p><p><img src=\"http://localhost:9090/files/b344314319f047cf9192ce64ca454674\" style=\"max-width:100%;\" contenteditable=\"false\"/></p>', '管理员', '2021-07-17 19:14:14');
-INSERT INTO `news` VALUES (6, '带你从0搭建一个Springboot+vue前后端分离项目，真的很简单！', '<p><iframe src=\"//player.bilibili.com/player.html?aid=803885504&amp;bvid=BV14y4y1M7Nc&amp;cid=361690131&amp;page=1\" scrolling=\"no\" border=\"0\" frameborder=\"no\" framespacing=\"0\" allowfullscreen=\"true\" width=\"600\" height=\"400\"> </iframe></p>', '管理员', '2021-09-05 22:28:25');
-INSERT INTO `news` VALUES (7, '我擦！妹子给我整害羞了⁄(⁄ ⁄ ⁄ω⁄ ⁄ ⁄)⁄……', '<p><iframe src=\"//player.bilibili.com/player.html?aid=717601909&amp;bvid=BV1sQ4y1174g&amp;cid=396410411&amp;page=1\" scrolling=\"no\" border=\"0\" frameborder=\"no\" framespacing=\"0\" allowfullscreen=\"true\" width=\"600\" height=\"400\"> </iframe></p>', '管理员', '2021-09-05 22:31:40');
+INSERT INTO `news` VALUES (4, '4444', '<p>444444</p><p><br/></p><p>44444</p><p><br/></p><p><br/></p><p>555555<img src=\"https://img1.baidu.com/it/u=4110196045,3829597861&amp;fm=26&amp;fmt=auto&amp;gp=0.jpg\" contenteditable=\"false\" style=\"font-size: 14px; max-width: 100%;\"/></p>', '管理员', '2020-05-13 18:55:27');
+INSERT INTO `news` VALUES (5, '77777', '<p>7777</p><p><br/></p><p><img src=\"http://localhost:9090/files/c536f6f2f0e94983951240d73d740601\" style=\"max-width:100%;\" contenteditable=\"false\" width=\"388\" height=\"388\"/><br/></p><p>8888</p><p><br/></p><p><img src=\"http://localhost:9090/files/b344314319f047cf9192ce64ca454674\" style=\"max-width:100%;\" contenteditable=\"false\"/></p>', '管理员', '2020-05-15 19:14:14');
 
 -- ----------------------------
 -- Table structure for t_order
 -- ----------------------------
 DROP TABLE IF EXISTS `t_order`;
 CREATE TABLE `t_order`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
   `total_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '总价',
   `pay_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '实付款',
-  `discount` decimal(10, 2) NULL DEFAULT 0.00 COMMENT '优惠金额',
-  `transport_price` decimal(10, 2) NULL DEFAULT 0.00 COMMENT '运费',
+  `discount` decimal(10, 2) NULL COMMENT '优惠金额',
+  `transport_price` decimal(10, 2) NULL COMMENT '运费',
   `order_no` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单编号',
-  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '用户id',
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户账户',
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  `pay_time` timestamp NULL DEFAULT NULL COMMENT '支付时间',
-  `state` int(1) NOT NULL DEFAULT 0 COMMENT '状态',
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `pay_time` timestamp(0) NULL DEFAULT NULL COMMENT '支付时间',
+  `state` int(0) NOT NULL DEFAULT 0 COMMENT '状态',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_order
 -- ----------------------------
-INSERT INTO `t_order` VALUES (14, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1434521785293873152', 1, 'admin', '2021-09-05 22:20:20', NULL, 0);
-INSERT INTO `t_order` VALUES (15, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1434522270700761088', 1, 'admin', '2021-09-05 22:22:42', '2021-09-05 22:22:37', 1);
-INSERT INTO `t_order` VALUES (16, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1434522590331891712', 1, 'admin', '2021-09-05 22:24:00', '2021-09-05 22:23:57', 1);
-INSERT INTO `t_order` VALUES (17, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1436963760559362048', 1, 'admin', '2021-09-12 16:03:52', NULL, 0);
+INSERT INTO `t_order` VALUES (14, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1434521785293873152', 1, 'admin', '2020-05-05 22:20:20', NULL, 0);
+INSERT INTO `t_order` VALUES (15, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1434522270700761088', 1, 'admin', '2020-05-05 15:04:52', '2020-05-05 22:22:37', 1);
+INSERT INTO `t_order` VALUES (16, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1434522590331891712', 1, 'admin', '2020-05-06 15:04:57', '2020-05-06 22:23:57', 1);
+INSERT INTO `t_order` VALUES (17, '明朝那些事', 60.00, 60.00, 0.00, 0.00, '1436963760559362048', 1, 'admin', '2020-05-08 16:03:52', NULL, 0);
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
   `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '昵称',
-  `age` int(11) NULL DEFAULT NULL COMMENT '年龄',
+  `age` int(0) NULL DEFAULT NULL COMMENT '年龄',
   `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '性别',
   `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '地址',
-  `role` int(11) NULL DEFAULT NULL COMMENT '角色，1：管理员，2：普通用户',
+  `role` int(0) NULL DEFAULT NULL COMMENT '角色，1：管理员，2：普通用户',
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '头像',
-  `account` decimal(10, 2) NULL DEFAULT 0.00 COMMENT '账户余额',
+  `account` decimal(10, 2) NULL COMMENT '账户余额',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户信息表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
